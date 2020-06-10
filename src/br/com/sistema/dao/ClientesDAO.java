dcd7504bd929bbe29baa61381e18d0e0f7804c1c
Alex <pereirasander33@gmail.com>
2020-06-10 11:29:48 -0300

    -Iniciado programação do módulo principal de vendas -Definido data do sistema como base da venda no módulo de vendas -Criado formulário de pagamentos(escopo)

--- src/br/com/sistema/dao/ClientesDAO.java
+++ src/br/com/sistema/dao/ClientesDAO.java
@@ -262,6 +262,43 @@ public class ClientesDAO {
         }
 
     }
+     
+     
+     //buscar cliente por CPF
+      public Clientes buscaporcpf(String cpf){
+        try {
+            String sql = "select * from tb_clientes where cpf = ?";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1,cpf);
+
+            ResultSet rs = stmt.executeQuery();
+            Clientes obj = new Clientes();
+            
+            if(rs.next()) {
+                obj.setId(rs.getInt("id"));
+                obj.setNome(rs.getString("nome"));
+                obj.setRg(rs.getString("rg"));
+                obj.setCpf(rs.getString("cpf"));
+                obj.setEmail(rs.getString("email"));
+                obj.setTelefone(rs.getString("telefone"));
+                obj.setCelular(rs.getString("celular"));
+                obj.setCep(rs.getString("cep"));
+                obj.setEndereco(rs.getString("endereco"));
+                obj.setNumero(rs.getInt("numero"));
+                obj.setComplemento(rs.getString("complemento"));
+                obj.setBairro(rs.getString("bairro"));
+                obj.setCidade(rs.getString("cidade"));
+                obj.setUf(rs.getString("estado"));
+            }
+            
+            return obj;
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"Cliente não encontrado");
+        }
+      return null;
+    }
+    
     
 
 }
