e2c4982fe56afc96eb621cec69a6a93d1fec571f
Alexlionman <55644831+Alexlionman@users.noreply.github.com>
2020-08-03 13:34:20 -0300

    Update ConnectionFactory.java

--- src/br/com/sistema/jdbc/ConnectionFactory.java
+++ src/br/com/sistema/jdbc/ConnectionFactory.java
@@ -10,7 +10,7 @@ import java.sql.DriverManager;
  * @author perei
  */
 public class ConnectionFactory {
-    
+    ///dados próprios da minha conexão local
     public Connection getConnection(){
         try {
             return DriverManager.getConnection("jdbc:mysql://127.0.0.1/bdvendas?allowPublicKeyRetrieval=true&useSSL=false","usuariocurso", "Alex4675");
